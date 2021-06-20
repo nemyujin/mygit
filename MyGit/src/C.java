e447ade1a860e4cc3261c4d6145119227d8ad7c5
홍유진 <@DESKTOP-0MT66Q0>
2021-06-21 02:04:56 +0900

    Commit and push

---
+++ MyGit/src/C.java
@@ -0,0 +1,11 @@
+
+public class C {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git!");
+		 System.out.println("This is C version");
+		 System.out.println("Hello");
+
+	}
+
+}
